89e40e04e31f01294c7522d0189256b71b0200a0
Arthur Scapin Pereira <arthurscapin19@gmail.com>
2024-05-05 16:21:19 -0300

    ajustes na Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,19 +8,19 @@ class Sidebar extends React.Component {
       <div id='sidebar'>
         <img id='logo' src="./assets/logo.png" alt="logo" />
         <div className='separator'></div>
-        <div>
+        <div className='item'>
           <box-icon name='home-circle' color='#636864' size='28px'></box-icon>
         </div>
-        <div>
+        <div className='item'>
           <box-icon name='bookmarks' color='#636864' size='28px'></box-icon>
         </div>
-        <div>
+        <div className='item'>
           <box-icon type='solid' name='videos' color='#636864' size='28px'></box-icon>
         </div>
-        <div>
+        <div className='item'>
           <box-icon name='cog' color='#636864' size='28px'></box-icon>
         </div>
-        <div>
+        <div className='item'>
           <box-icon name='log-out' color='#636864' size='28px'></box-icon>
         </div>
       </div>
