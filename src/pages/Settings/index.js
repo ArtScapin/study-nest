ffa00c5ef7bd03dd180ec55c79fddbe607207571
Arthur Scapin Pereira <arthurscapin19@gmail.com>
2024-05-07 19:56:42 -0300

    listagem de cursos do usuário

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -1,8 +1,36 @@
 import Header from '../../components/Header';
 import Sidebar from '../../components/Sidebar';
 import './style.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Settings() {
+  const [courses, setCourses] = useState([]);
+  const [isLoading, setLoading] = useState(true); 
+
+  const handleRedirect = (route) => {
+    window.location.href = `settings/${route}`;
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem('authToken');
+    axios.get('http://localhost:3333/course/my',
+      {
+        headers: { 
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      }            
+    )
+    .then(({data}) => {
+      setLoading(false)
+      setCourses(data)
+    })
+    .catch(error => {
+      console.error('Erro ao buscar dados da API:', error);
+    });
+  });
+
   return (
     <>
       <Sidebar></Sidebar>
@@ -11,11 +39,13 @@ export default function Settings() {
         <div id='main'>
           <div className='table'>
             <div>My Courses</div>
-            <div>lorem ipsum sit amet</div>
-            <div>lorem ipsum sit amet</div>
-            <div>lorem ipsum sit amet</div>
-            <div>lorem ipsum sit amet</div>
-            <div>lorem ipsum sit amet</div>
+            {
+              isLoading ?
+              <div>Loading...</div>:
+              courses.map(({id, name}) => {
+                return <div key={id} onClick={() => handleRedirect(`course?id=${id}`)}>{name}</div>
+            })
+            }
           </div>
         </div>
       </div>
