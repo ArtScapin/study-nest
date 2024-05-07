364690569741b478d82b278356bf80d0d9e86aca
Arthur Scapin Pereira <arthurscapin19@gmail.com>
2024-05-07 15:00:21 -0300

    ajustes nos links da Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,20 +8,24 @@ export default function Sidebar () {
     window.location.reload();
   }
 
+  const handleRedirect = (route) => {
+    window.location.href = `${route}`;
+  };
+
   return (
     <div id='sidebar'>
       <img id='logo' src="./assets/logo.png" alt="logo" />
       <div className='separator'></div>
-      <div className='item'>
+      <div className='item' onClick={() => handleRedirect('/home')}>
         <box-icon name='home-circle' color='#636864' size='28px'></box-icon>
       </div>
-      <div className='item'>
+      <div className='item' onClick={() => handleRedirect('/my-area')}>
         <box-icon name='bookmarks' color='#636864' size='28px'></box-icon>
       </div>
-      <div className='item'>
+      <div className='item' onClick={() => handleRedirect('/courses')}>
         <box-icon type='solid' name='videos' color='#636864' size='28px'></box-icon>
       </div>
-      <div className='item'>
+      <div className='item' onClick={() => handleRedirect('/settings')}>
         <box-icon name='cog' color='#636864' size='28px'></box-icon>
       </div>
       <div className='item' onClick={handleLoggout}>
