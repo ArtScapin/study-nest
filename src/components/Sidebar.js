89e0328f6b37740dafcc219994dbb183d800a3a9
Arthur Scapin Pereira <arthurscapin19@gmail.com>
2024-05-07 14:40:07 -0300

    Loggout

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,30 +2,31 @@ import React from 'react';
 import './Sidebar.css'; // Importa os estilos CSS
 import 'boxicons'
 
-class Sidebar extends React.Component {
-    render() {
-    return (
-      <div id='sidebar'>
-        <img id='logo' src="./assets/logo.png" alt="logo" />
-        <div className='separator'></div>
-        <div className='item'>
-          <box-icon name='home-circle' color='#636864' size='28px'></box-icon>
-        </div>
-        <div className='item'>
-          <box-icon name='bookmarks' color='#636864' size='28px'></box-icon>
-        </div>
-        <div className='item'>
-          <box-icon type='solid' name='videos' color='#636864' size='28px'></box-icon>
-        </div>
-        <div className='item'>
-          <box-icon name='cog' color='#636864' size='28px'></box-icon>
-        </div>
-        <div className='item'>
-          <box-icon name='log-out' color='#636864' size='28px'></box-icon>
-        </div>
-      </div>
-    );
+export default function Sidebar () {
+  const handleLoggout = () => {
+    localStorage.setItem('authToken', undefined);
+    window.location.reload();
   }
-}
 
-export default Sidebar;
+  return (
+    <div id='sidebar'>
+      <img id='logo' src="./assets/logo.png" alt="logo" />
+      <div className='separator'></div>
+      <div className='item'>
+        <box-icon name='home-circle' color='#636864' size='28px'></box-icon>
+      </div>
+      <div className='item'>
+        <box-icon name='bookmarks' color='#636864' size='28px'></box-icon>
+      </div>
+      <div className='item'>
+        <box-icon type='solid' name='videos' color='#636864' size='28px'></box-icon>
+      </div>
+      <div className='item'>
+        <box-icon name='cog' color='#636864' size='28px'></box-icon>
+      </div>
+      <div className='item' onClick={handleLoggout}>
+        <box-icon name='log-out' color='#636864' size='28px'></box-icon>
+      </div>
+    </div>
+  );
+}
